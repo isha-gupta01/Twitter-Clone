3f778391c6a48c275040ec98ce08aa2ad17326e4
Bloomer004 <ishaguptavnsv@gmail.com>
2025-04-14 16:59:57 +0530

    committed before correcting bio thing

--- app/ProfilePage/page.js
+++ app/ProfilePage/page.js
@@ -1,39 +1,43 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import FirstSec from '@/components/FirstSec'
 import ThirdSec from '@/components/ThirdSec'
 import Profile from '@/components/Profile'
-// import { useState } from 'react'
 import ProtectedRoute from '@/components/ProtectedRoute'
 import MobFirstSec from '@/components/MobFirstSec'
 
 const ProfilePage = () => {
-  // const [activeComponent, setActiveComponent] = useState()
-  const user = JSON.parse(localStorage.getItem("user"));
-  const userId = user?.userId;
-  
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      const parsedUser = JSON.parse(storedUser);
+      setUserId(parsedUser?.userId);
+    }
+  }, []);
+
   return (
-    <div>
-      <ProtectedRoute>
-        <div className='black text-white'>
-          <div className="flex md:container mx-auto  ">
-
-            <div className='hidden md:flex'>
-              <FirstSec />
-            </div>
-            <div className='md:hidden block'>
-              <MobFirstSec />
-            </div>
-            <div className=" w-px bg-gray-400 hidden md:flex opacity-30 sticky left-[5.7rem] z-50"></div>
-            <Profile userId={userId}/>
-            <div className=" w-px bg-gray-400 opacity-25"></div>
-            <ThirdSec />
-
+    <ProtectedRoute>
+      <div className="black text-white">
+        <div className="flex md:container mx-auto">
+          <div className="hidden md:flex">
+            <FirstSec />
           </div>
+          <div className="md:hidden block">
+            <MobFirstSec />
+          </div>
+          <div className="w-px bg-gray-400 hidden md:flex opacity-30 sticky left-[5.7rem] z-50"></div>
+          
+          {/* Only render Profile if userId is available */}
+          {userId && <Profile userId={userId} />}
+
+          <div className="w-px bg-gray-400 opacity-25"></div>
+          <ThirdSec />
         </div>
-      </ProtectedRoute>
-    </div>
-  )
+      </div>
+    </ProtectedRoute>
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
