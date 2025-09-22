59cfc12e82cc200905e6c2b54513f0c6f98963d4
Bloomer004 <ishaguptavnsv@gmail.com>
2025-09-22 15:01:14 +0530

    Skeleton

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,7 @@
+"use client"
+import { useEffect } from "react";
+import jwt_decode from "jwt-decode";
+import { useRouter } from "next/navigation";
 import "./globals.css";
 import SessionWrapper from "@/components/sesssionwrapper";
 import { Roboto } from 'next/font/google';
@@ -7,7 +11,7 @@ const roboto = Roboto({
   weight: ['400', '500', '700'], // choose weights you need
 });
 
-export const metadata = {
+const metadata = {
   title: "TwitterOn",
   description: "A Clone of Twitter. Made by Isha Gupta ❤️",
   icons: {
@@ -16,6 +20,26 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      try {
+        const decoded = jwt_decode(token);
+        const now = Date.now() / 1000;
+        if (decoded.exp < now) {
+          localStorage.removeItem("token");
+          localStorage.removeItem("user");
+          router.push("/login"); // optional redirect
+        }
+      } catch (err) {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        router.push("/login");
+      }
+    }
+  }, []);
   return (
     <html lang="en" className="dark">
       <head>
@@ -27,7 +51,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${roboto.className} antialiased`}
       >
-       <SessionWrapper>{children}</SessionWrapper> 
+        <SessionWrapper>{children}</SessionWrapper>
       </body>
     </html>
   );
